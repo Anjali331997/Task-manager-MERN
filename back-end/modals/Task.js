e647dfe934682e3a9b0251ef638ebd3fb8a738a2
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-10 10:20:29 +0530

    task schema updated

--- back-end/modals/Task.js
+++ back-end/modals/Task.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-
+    description: { typpe: String, required: true },
+    completed: { type: Boolean, default: false },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
 })
 
 const Task = mongoose.model('task', taskSchema);
