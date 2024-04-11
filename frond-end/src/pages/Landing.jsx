8d9f015edf556b3773fbd0ff2060e2193cdadfad
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-11 21:51:49 +0530

    Landing page

--- frond-end/src/pages/Landing.jsx
+++ frond-end/src/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Landing.css'
 
 const Landing = () => {
   return (
-    <div>
-        
+    <div className='landing_div'>
+      <div className="image_svg"></div>
+      <div className="signup_login"></div>
     </div>
   )
 }
