0439a4a73fd8e3e965c10f55eaf53d31aaf16f3a
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-13 00:07:46 +0530

    add task css added

--- frond-end/src/components/addTaskButton/AddTaskbutton.jsx
+++ frond-end/src/components/addTaskButton/AddTaskbutton.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
-import {AddTask} from '../../pages/AddTask/AddTask'
+import { AddTask } from '../../pages/AddTask/AddTask'
 
 const AddTaskbutton = () => {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const handleClose = () => setShowModal(false);
-    const handleShow = () => setShowModal(true);
-  
-    const handleSubmit = (formData) => {
-      // Save formData to state or perform any other action
-      console.log(formData);
-      handleClose();
-    };
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const handleSubmit = (formData) => {
+    // Save formData to state or perform any other action
+    console.log(formData);
+    handleClose();
+  };
   return (
     <div>
-      <Button variant="primary" onClick={handleShow}>
+      <Button variant="primary"
+        onClick={handleShow}
+        style={{
+          backgroundColor: "var(--violet)",
+          color: "var(--black)",
+          fontWeight: "500",
+          border: "0px"
+        }}>
         Add Task
       </Button>
       <AddTask
