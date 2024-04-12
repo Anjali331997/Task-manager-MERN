3de0c36dde799ee48f879b2d11393fb1163c246d
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-13 00:01:58 +0530

    Dashboard design

--- frond-end/src/App.js
+++ frond-end/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import Landing from './pages/landing/Landing.jsx';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
- 
+
 
   return (
     <>
-    <Landing/>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </>
   );
 }
---
+++ frond-end/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Dashboard.css'
+import AddTaskbutton from '../../components/addTaskButton/AddTaskbutton'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <AddTaskbutton/>
+    </div>
+  )
+}
+
+export default Dashboard
