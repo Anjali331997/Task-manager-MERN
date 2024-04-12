078d2257e094fa2d2a73f76f358f93f345c45e07
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-12 11:37:42 +0530

    landing page completed and css done

--- frond-end/src/components/Sign/Signup.jsx
+++ frond-end/src/components/Sign/Signup.jsx
@@ -4,7 +4,7 @@ import './Signup.css'
 const Signup = () => {
     return (
         <form>
-            <h2>Sign Up</h2>
+            <h2 className='header-signup'>Sign Up</h2>
             <input type='text' placeholder='Enter you name' />
             <input type='email' placeholder='username@email.com' />
             <input type='password' placeholder='password' />
