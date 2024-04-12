c0ad9994296ffa7f908351d2a3f2d07195b346a9
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-13 00:41:06 +0530

    designing header

--- frond-end/src/components/Header/Header.jsx
+++ frond-end/src/components/Header/Header.jsx
@@ -4,9 +4,11 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <div className='header_name'>Task Manager</div>
+            <div className='header_name'>
+                <h4>Task Manager</h4>
+            </div>
             <div className='profile'>
-                <h3>User Name</h3>
+                <p>User Name</p>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwcCMK76hlQuMHQO1sC30jBgPCnJuwIN9bmw&usqp=CAU" alt="profile_icon" />
             </div>
         </div>
